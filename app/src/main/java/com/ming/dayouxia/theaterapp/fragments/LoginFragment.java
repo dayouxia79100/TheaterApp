3e9f8ac458596026d8259bea64c9d5c2fbc8b1ec
ming <han.568@osu.edu>
2014-10-15 17:24:35 -0400

    changed the password input mask

--- app/src/main/java/com/ming/dayouxia/theaterapp/fragments/LoginFragment.java
+++ app/src/main/java/com/ming/dayouxia/theaterapp/fragments/LoginFragment.java
@@ -2,19 +2,49 @@ package com.ming.dayouxia.theaterapp.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.ming.dayouxia.theaterapp.R;
 
 
 public class LoginFragment extends Fragment {
 
+    private EditText mPasswordField;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        mPasswordField = (EditText)rootView.findViewById(R.id.password_login_text);
+        mPasswordField.setTransformationMethod(new AsteriskPasswordTransformationMethod());
         return rootView;
     }
+
+    private class AsteriskPasswordTransformationMethod extends PasswordTransformationMethod {
+        @Override
+        public CharSequence getTransformation(CharSequence source, View view) {
+            return new PasswordCharSequence(source);
+        }
+    }
+
+    private class PasswordCharSequence implements CharSequence {
+        private CharSequence mSource;
+        public PasswordCharSequence(CharSequence source) {
+            mSource = source; // Store char sequence
+        }
+        public char charAt(int index) {
+            return '*'; // This is the important part
+        }
+        public int length() {
+            return mSource.length(); // Return default
+        }
+        public CharSequence subSequence(int start, int end) {
+            return mSource.subSequence(start, end); // Return default
+        }
+    }
+
+
 }
--- app/src/main/java/com/ming/dayouxia/theaterapp/fragments/LoginFragmentDialogV2.java
+++ app/src/main/java/com/ming/dayouxia/theaterapp/fragments/LoginFragmentDialogV2.java
@@ -5,23 +5,52 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 import com.ming.dayouxia.theaterapp.R;
 
 
 public class LoginFragmentDialogV2 extends DialogFragment {
 
+
+    private EditText mPasswordField;
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
-        return builder.setView(inflater.inflate(R.layout.fragment_login, null))
+        View v = inflater.inflate(R.layout.fragment_login, null);
+        mPasswordField = (EditText) v.findViewById(R.id.password_login_text);
+        mPasswordField.setTransformationMethod(new AsteriskPasswordTransformationMethod());
+
+        return builder.setView(v)
                 .create();
+    }
 
+    private class AsteriskPasswordTransformationMethod extends PasswordTransformationMethod {
+        @Override
+        public CharSequence getTransformation(CharSequence source, View view) {
+            return new PasswordCharSequence(source);
+        }
+    }
 
-
+    private class PasswordCharSequence implements CharSequence {
+        private CharSequence mSource;
+        public PasswordCharSequence(CharSequence source) {
+            mSource = source; // Store char sequence
+        }
+        public char charAt(int index) {
+            return '*'; // This is the important part
+        }
+        public int length() {
+            return mSource.length(); // Return default
+        }
+        public CharSequence subSequence(int start, int end) {
+            return mSource.subSequence(start, end); // Return default
+        }
     }
 }
